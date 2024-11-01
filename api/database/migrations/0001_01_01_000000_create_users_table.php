2088c4e503475648c2b07cca93bca40ff9578932
dmKopylets <dm.kopylets@gmail.com>
2024-11-01 16:44:39 +0200

    small fix # 3

--- api/database/migrations/0001_01_01_000000_create_users_table.php
+++ api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,19 +11,18 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->bigInteger('chat_id')->unique()->after('id');
-//            $table->string('email')->unique();
-//            $table->timestamp('email_verified_at')->nullable();
-//            $table->string('password');
-//            $table->rememberToken();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
 
-//        Schema::create('password_reset_tokens', function (Blueprint $table) {
-//            $table->string('email')->primary();
-//            $table->string('token');
-//            $table->timestamp('created_at')->nullable();
-//        });
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
@@ -38,7 +37,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-//        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };
